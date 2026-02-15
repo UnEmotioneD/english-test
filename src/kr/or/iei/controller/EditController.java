a449ff796980649b0faea9e5168d7e5749990d9f
UnEmotioneD <blackeagle10@icloud.com>
2026-02-16 08:16:10 +0900

    fix: add scanner to EditController

--- src/kr/or/iei/controller/EditController.java
+++ src/kr/or/iei/controller/EditController.java
@@ -7,12 +7,15 @@ import kr.or.iei.viewer.Viewer;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class EditController {
+    Scanner sc;
     MenuController menuCon;
     Viewer viewer;
 
     public EditController(MenuController menuCon) {
+        sc = new Scanner(System.in);
         this.menuCon = menuCon;
         this.viewer = new Viewer();
     }
