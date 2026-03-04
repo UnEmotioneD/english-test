0603feb613a348688f5e9965907f9816286cd9c4
UnEmotioneD <blackeagle10@icloud.com>
2026-03-04 17:17:16 +0900

    format: indentation 2 spaces

--- src/kr/or/iei/controller/MenuController.java
+++ src/kr/or/iei/controller/MenuController.java
@@ -1,65 +1,64 @@
 package kr.or.iei.controller;
 
+import java.util.ArrayList;
 import kr.or.iei.common.Config;
 import kr.or.iei.common.Util;
 import kr.or.iei.model.vo.Word;
 import kr.or.iei.viewer.Viewer;
 
-import java.util.ArrayList;
-
 public class MenuController {
-    Viewer viewer;
-    SearchController searchCon;
-    EditController editCon;
-    TestController testCon;
+  Viewer viewer;
+  SearchController searchCon;
+  EditController editCon;
+  TestController testCon;
 
-    Util util;
-    ArrayList<Word> wordList;
+  Util util;
+  ArrayList<Word> wordList;
 
-    public MenuController() {
-        viewer = new Viewer();
-        searchCon = new SearchController(this);
-        editCon = new EditController(this);
-        testCon = new TestController(this);
+  public MenuController() {
+    viewer = new Viewer();
+    searchCon = new SearchController(this);
+    editCon = new EditController(this);
+    testCon = new TestController(this);
 
-        util = new Util();
-        wordList = new ArrayList<>();
+    util = new Util();
+    wordList = new ArrayList<>();
+  }
+
+  public void mainMenu() {
+    while (true) {
+      readWordFile();
+      int menu = viewer.menu();
+      switch (menu) {
+        case 1:
+          searchCon.search();
+          break;
+        case 2:
+          editCon.add();
+          break;
+        case 3:
+          editCon.edit();
+          break;
+        case 4:
+          testCon.test();
+          break;
+        case 5:
+          testCon.reTest();
+          break;
+        case 0:
+          viewer.terminated();
+          return;
+        default:
+          break;
+      }
     }
+  }
 
-    public void mainMenu() {
-        while (true) {
-            readWordFile();
-            int menu = viewer.menu();
-            switch (menu) {
-                case 1:
-                    searchCon.search();
-                    break;
-                case 2:
-                    editCon.add();
-                    break;
-                case 3:
-                    editCon.edit();
-                    break;
-                case 4:
-                    testCon.test();
-                    break;
-                case 5:
-                    testCon.reTest();
-                    break;
-                case 0:
-                    viewer.terminated();
-                    return;
-                default:
-                    break;
-            }
-        }
-    }
+  public void readWordFile() {
+    wordList = util.readFile(Config.WORD_FILE);
+  }
 
-    public void readWordFile() {
-        wordList = util.readFile(Config.WORD_FILE);
-    }
-
-    public ArrayList<Word> getWordList() {
-        return wordList;
-    }
+  public ArrayList<Word> getWordList() {
+    return wordList;
+  }
 }
--- src/kr/or/iei/controller/SearchController.java
+++ src/kr/or/iei/controller/SearchController.java
@@ -1,81 +1,80 @@
 package kr.or.iei.controller;
 
+import java.util.ArrayList;
 import kr.or.iei.model.vo.Word;
 import kr.or.iei.viewer.SearchViewer;
 
-import java.util.ArrayList;
-
 public class SearchController {
-    MenuController menuCon;
-    SearchViewer searchViewer;
+  MenuController menuCon;
+  SearchViewer searchViewer;
 
-    public SearchController(MenuController menuCon) {
-        this.menuCon = menuCon;
-        this.searchViewer = new SearchViewer();
-    }
+  public SearchController(MenuController menuCon) {
+    this.menuCon = menuCon;
+    this.searchViewer = new SearchViewer();
+  }
 
-    public void search() {
-        String searchWord;
-        final String cancelSearch = "C";
-        boolean searching = true;
+  public void search() {
+    String searchWord;
+    final String cancelSearch = "C";
+    boolean searching = true;
 
-        while (searching) {
-            searchWord = searchViewer.searchViewer(cancelSearch);
+    while (searching) {
+      searchWord = searchViewer.searchViewer(cancelSearch);
 
-            if (searchWord.equals(cancelSearch)) {
-                searching = false;
-                searchViewer.cancelSearch();
-                continue;
-            }
+      if (searchWord.equals(cancelSearch)) {
+        searching = false;
+        searchViewer.cancelSearch();
+        continue;
+      }
 
-            ArrayList<Word> wordList = searchWord(searchWord);
+      ArrayList<Word> wordList = searchWord(searchWord);
 
-            if (!wordList.isEmpty()) {
-                searchViewer.searchResultsHeader();
-                for (Word word : wordList) {
-                    searchViewer.showSearchResults(word);
-                }
-            } else {
-                searchViewer.noSearchResults(searchWord);
-            }
+      if (!wordList.isEmpty()) {
+        searchViewer.searchResultsHeader();
+        for (Word word : wordList) {
+          searchViewer.showSearchResults(word);
         }
+      } else {
+        searchViewer.noSearchResults(searchWord);
+      }
     }
+  }
 
-    public ArrayList<Word> searchWord(String searchWord) {
-        ArrayList<Word> searchResults = new ArrayList<>();
-        Word searchResult;
-        int searchWordLength = searchWord.length();
+  public ArrayList<Word> searchWord(String searchWord) {
+    ArrayList<Word> searchResults = new ArrayList<>();
+    Word searchResult;
+    int searchWordLength = searchWord.length();
 
-        for (Word word : menuCon.getWordList()) {
-            String wordFromFile = word.getWord();
-            int wordFromFileLength = wordFromFile.length();
+    for (Word word : menuCon.getWordList()) {
+      String wordFromFile = word.getWord();
+      int wordFromFileLength = wordFromFile.length();
 
-            if (searchWordLength > wordFromFileLength) {
-                continue;
-            } else if (searchWordLength == wordFromFileLength) {
-                if (searchWord.equalsIgnoreCase(wordFromFile)) {
-                    searchResult = new Word();
-                    searchResult.setWord(word.getWord());
-                    searchResult.setDef1(word.getDef1());
-                    searchResult.setDef2(word.getDef2());
-                    searchResults.add(searchResult);
-                }
-            } else if (searchWordLength < wordFromFileLength) {
-                for (int i = 0; i < wordFromFileLength - searchWordLength + 1; i++) {
-                    String subStr = wordFromFile.substring(i, i + searchWordLength);
-                    if (searchWord.equalsIgnoreCase(subStr)) {
-                        searchResult = new Word();
-                        searchResult.setWord(word.getWord());
-                        searchResult.setDef1(word.getDef1());
-                        searchResult.setDef2(word.getDef2());
-                        searchResults.add(searchResult);
-                        break;
-                    }
-                }
-            }
+      if (searchWordLength > wordFromFileLength) {
+        continue;
+      } else if (searchWordLength == wordFromFileLength) {
+        if (searchWord.equalsIgnoreCase(wordFromFile)) {
+          searchResult = new Word();
+          searchResult.setWord(word.getWord());
+          searchResult.setDef1(word.getDef1());
+          searchResult.setDef2(word.getDef2());
+          searchResults.add(searchResult);
         }
-        return searchResults;
+      } else if (searchWordLength < wordFromFileLength) {
+        for (int i = 0; i < wordFromFileLength - searchWordLength + 1; i++) {
+          String subStr = wordFromFile.substring(i, i + searchWordLength);
+          if (searchWord.equalsIgnoreCase(subStr)) {
+            searchResult = new Word();
+            searchResult.setWord(word.getWord());
+            searchResult.setDef1(word.getDef1());
+            searchResult.setDef2(word.getDef2());
+            searchResults.add(searchResult);
+            break;
+          }
+        }
+      }
     }
+    return searchResults;
+  }
 
-    public void searchDef() {}
+  public void searchDef() {}
 }
